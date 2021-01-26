922de06106e4511c6817dbb1fa2c275c3009546c
booldook <booldook@gmail.com>
2021-01-26 17:22:59 +0900

    ....

--- routes/board-route.js
+++ routes/board-route.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const pugs = { css: 'board', js: 'board', title: 'Express Board', headerTitle: 'Node/Express를 활용한 게시판' }
+const pugs = { 
+	css: 'board', 
+	js: 'board', 
+	title: 'Express Board', 
+	headerTitle: 'Node/Express를 활용한 게시판' 
+}
+
 router.get('/', (req, res, next) => {
-	const pug = { ...pugs };
-	res.render('board/list', pug);
-})
+	res.render('board/list', { ...pugs });
+});
+
+router.get('/create', (req, res, next) => {
+	res.render('board/create', pugs);
+});
 
 module.exports = router;
