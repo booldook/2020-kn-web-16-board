188ed180bda5102308d685cd0e0c21c7703c61cd
booldook <booldook@gmail.com>
2021-01-27 12:22:56 +0900

    /board/save

---
+++ public/js/board.js
@@ -0,0 +1,8 @@
+function onSave(f) {
+	if(f.title.value.trim() == "") {
+		alert('제목을 입력하세요.');
+		f.title.focus();
+		return false;
+	}
+	return true;
+}
--- routes/board-route.js
+++ routes/board-route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { pool } = require('../modules/mysql-pool');
+const { err } = require('../modules/util');
 const pugs = { 
 	css: 'board', 
 	js: 'board', 
@@ -14,6 +16,18 @@ router.get('/', (req, res, next) => {
 router.get('/create', (req, res, next) => {
 	const pug = { ...pugs, tinyKey: 'po1ip1xxy0zm6ou4bk6qpelj7vno5nvg9w1qssyr2a0inkks' }
 	res.render('board/create', pug);
+});
+
+router.post('/save', async (req, res) => {
+	try {
+		const sql = '';
+		const value = [];
+		const r = await pool.query(sql, value);
+		res.json(r[0]);
+	}
+	catch(e) {
+		next(err(e));
+	}
 });
 
 module.exports = router;
