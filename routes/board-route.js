6c9ec82887309a7ea536d8a2f8fba7f46cb6890b
booldook <booldook@gmail.com>
2021-01-28 11:08:56 +0900

    ...

--- routes/board-route.js
+++ routes/board-route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { upload } = require('../modules/multers');
 const { pool } = require('../modules/mysql-pool');
 const { err, alert } = require('../modules/util');
+const moment = require('moment');
+const path = require('path');
 const pugs = { 
 	css: 'board', 
 	js: 'board', 
@@ -10,8 +12,26 @@ const pugs = {
 	headerTitle: 'Node/Express를 활용한 게시판' 
 }
 
-router.get('/', (req, res, next) => {
-	res.render('board/list', { ...pugs });
+router.get('/', async (req, res, next) => {
+	try {
+		let sql = 'SELECT * FROM board ORDER BY id DESC';
+		const r = await pool.query(sql);
+		const rs = r[0].map((v) => {
+			v.wdate = moment(v.wdate).format('YYYY-MM-DD');
+			if(v.savefile) {
+				let ext = path.extname(v.savefile).substr(1).toLowerCase();
+				ext = (ext == 'jpeg') ? 'jpg': ext;
+				ext = ext.substr(0, 3);
+				v.icon = `/img/ext/${ext}.png`;
+			}
+			else v.icon = '/img/empty.png';
+			return v;
+		});
+		res.render('board/list', { ...pugs, rs });
+	}
+	catch(e) {
+		next(err(e.message));
+	}
 });
 
 router.get('/create', (req, res, next) => {
@@ -37,7 +57,7 @@ router.post('/save', upload.single('upfile'), async (req, res, next) => {
 		}
 	}
 	catch(e) {
-		next(err(e));
+		next(err(e.message));
 	}
 });
 
