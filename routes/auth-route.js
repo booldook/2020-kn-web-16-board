1a99c841cd630a9248cd5e495fc2a228208356e0
booldook <booldook@gmail.com>
2021-01-29 15:27:07 +0900

    ...

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const pugs = {
+	css: 'auth', 
+	js: 'auth', 
+	title: 'Express Board', 
+	headerTitle: 'Node/Express를 활용한 인증 구현' 
+}
 
+router.get('/join', (req, res, next) => {
+	const pug = { ...pugs };
+	pug.headerTitle += ' - 회원가입';
+	res.render('auth/join', { ...pug });
+});
 
 module.exports = router;
