b4b6f2035df6e19c73f717c7d84ca297e14c5eb8
booldook <booldook@gmail.com>
2021-01-29 17:15:30 +0900

    auth 진행중

--- public/js/auth.js
+++ public/js/auth.js
@@ -5,13 +5,13 @@ function comment(el, cmt, cls) {
 }
 
 function onBlurId(el) {
+	function onResponse(r) {
+		if(r.result) comment(el, '멋진 아이디입니다. 사용가능합니다.', 'active');
+		else comment(el, '존재하는 아이디입니다. 사용할 수 없습니다.', 'danger');
+	}
 	var userid = $(el).val().trim();	// el.value
-	if(userid.length < 8) {
-		comment(el, '아이디는 8자 이상입니다.', 'danger');
-	}
-	else {
-		
-	}
+	if(userid.length < 8) comment(el, '아이디는 8자 이상입니다.', 'danger');
+	else $.get('/auth/userid', { userid: userid }, onResponse);
 }
 
 function onBlurPw(el) {
--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { pool } = require('../modules/mysql-pool');
 const router = express.Router();
 
 const pugs = {
@@ -13,5 +14,21 @@ router.get('/join', (req, res, next) => {
 	pug.headerTitle += ' - 회원가입';
 	res.render('auth/join', { ...pug });
 });
+
+/*********** API ***********/
+router.get('/userid', async (req, res, next) => {
+	try {
+		let sql, value, r, rs;
+		sql = 'SELECT userid FROM auth WHERE userid=?';
+		value = [req.query.userid];
+		r = await pool.query(sql, value);
+		if(r[0][0]) res.json({ code: 200, result: false });
+		else res.json({ code: 200, result: true });
+	}
+	catch(e) {
+		next(err(e.message));
+	}
+});
+
 
 module.exports = router;
