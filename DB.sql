a520af3e694a60a3d863bc73d741f273b135a795
booldook <booldook@gmail.com>
2021-01-29 15:51:10 +0900

    ...

--- DB.sql
+++ DB.sql
@@ -17,6 +17,20 @@
 CREATE DATABASE IF NOT EXISTS `booldook` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `booldook`;
 
+-- 테이블 booldook.auth 구조 내보내기
+CREATE TABLE IF NOT EXISTS `auth` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `userid` varchar(50) NOT NULL,
+  `passwd` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `grade` tinyint unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
 -- 테이블 booldook.board 구조 내보내기
 CREATE TABLE IF NOT EXISTS `board` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
@@ -29,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `board` (
   `readnum` int unsigned NOT NULL DEFAULT '0',
   `uid` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
