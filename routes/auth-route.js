798812e349b475624034addba2cb37d2bad04726
booldook <booldook@gmail.com>
2021-02-01 13:13:50 +0900

    /auth/save

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { pool } = require('../modules/mysql-pool');
+const { err } = require('../modules/util');
 const router = express.Router();
 
 const pugs = {
@@ -8,6 +9,36 @@ const pugs = {
 	title: 'Express Board', 
 	headerTitle: 'Node/Express를 활용한 인증 구현' 
 }
+
+router.post('/save', async (req, res, next) => {
+	try {
+		let { userid, userpw, username, email } = req.body;
+		let sql, value, r, rs;
+		let len = userpw.length;
+		let num = userpw.search(/[0-9]/g);
+		let eng = userpw.search(/[a-z]/ig);
+		let spe = userpw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+		let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+		sql = 'SELECT userid FROM auth WHERE userid=?';
+		value = [userid];
+		r = await pool.query(sql, value);
+		rs = r[0];
+		if(rs.length > 0) next(err('아이디 중복 오류'));
+		if(len<8 || len>20 || num<0 || eng<0 || spe<0) next(err('비밀번호 오류'));
+		if(username.length == 0) next(err('이름 오류'));
+		if(email.match(regExp) == null) next(err('이메일 오류'));
+
+		sql = 'INSERT INTO auth SET userid=?, userpw=?, username=?, email=?';
+		value = [userid, userpw, username, email];
+		r = await pool.query(sql, value);
+		rs = r[0];
+		if(rs.affectedRows == 1) res.redirect('/');
+		else next(err('회원등록 오류'));
+	}
+	catch(e) {
+		next(err(e.message));
+	}
+});
 
 router.get('/join', (req, res, next) => {
 	const pug = { ...pugs };
