c214f81909be3e6b48cc28571dc645c3b7a3853b
booldook <booldook@gmail.com>
2021-02-05 12:05:46 +0900

    ...

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,18 +1,22 @@
 function onPlus(el) {
-	var html = ``;
-	html += '<div class="list-wrap file-wrap">';
-	html += '<div class="title">첨부이미지</div>';
-	html += '<div class="list">';
-	html += '<input class="form-control-file" type="file" name="upfile">';
-	html += '</div>';
-	html += '<div class="bts">';
-	html += '<i class="fa fa-minus-circle text-danger" onclick="onMinus(this)"></i>';
-	html += '</div>';
-	html += '</div>';
-	$(".file-wrapper").append(html);
+	if($(".file-wrapper .file-wrap").length < 9) {
+		var html = ``;
+		html += '<div class="list-wrap file-wrap">';
+		html += '<div class="title">첨부이미지</div>';
+		html += '<div class="list">';
+		html += '<input class="form-control-file" type="file" name="upfile">';
+		html += '</div>';
+		html += '<div class="bts">';
+		html += '<i class="fa fa-minus-circle text-danger" onclick="onMinus(this)"></i>';
+		html += '</div>';
+		html += '</div>';
+		$(".file-wrapper").append(html);
+	}
+	if($(".file-wrap").length >= 10) $(".file-wrap").eq(0).find("a").hide();
 }
 
 function onMinus(el) {
 	$(el).parent().parent().remove();
+	$(".file-wrap").eq(0).find("a").show();
 }
 
